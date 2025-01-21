e5b1041532f701107faec45f24c6535763dd4ee8
derick_vitor <derick.castro@primecontrol.com.br>
2025-01-21 19:33:10 -0300

    fix render deploy

--- searchcnpj/settings.py
+++ searchcnpj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["searchcnpjapp-334834346634.us-central1.run.app", "https://search-cnpj.onrender.com"]
+ALLOWED_HOSTS = ["searchcnpjapp-334834346634.us-central1.run.app", "search-cnpj.onrender.com"]
 
 
 # Application definition
